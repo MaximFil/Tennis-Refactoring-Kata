2ed1059417a079bf423c96a2f511ead618154480
DESKTOP-17MGHM3\MAXFI <maks1998mops@mail.ru>
2024-04-20 10:59:38 +0300

    refactor class

--- csharp/Tennis/TennisGame2.cs
+++ csharp/Tennis/TennisGame2.cs
@@ -1,107 +1,110 @@
+using System;
+using System.Text;
+using static System.Formats.Asn1.AsnWriter;
+
 namespace Tennis
 {
     public class TennisGame2 : ITennisGame
     {
-        private int p1point;
-        private int p2point;
+        private int _p1point;
+        private int _p2point;
 
-        private string p1res = "";
-        private string p2res = "";
-        private string player1Name;
-        private string player2Name;
+        private string _p1res = "";
+        private string _p2res = "";
+        private string _player1Name;
+        private string _player2Name;
 
         public TennisGame2(string player1Name, string player2Name)
         {
-            this.player1Name = player1Name;
-            p1point = 0;
-            this.player2Name = player2Name;
+            _player1Name = player1Name;
+            _player2Name = player2Name;
         }
 
         public string GetScore()
         {
-            var score = "";
-            if (p1point == p2point && p1point < 3)
-            {
-                if (p1point == 0)
-                    score = "Love";
-                if (p1point == 1)
-                    score = "Fifteen";
-                if (p1point == 2)
-                    score = "Thirty";
-                score += "-All";
-            }
-            if (p1point == p2point && p1point > 2)
-                score = "Deuce";
+            var score = String.Empty;
 
-            if (p1point > 0 && p2point == 0)
+            if (_p1point == _p2point && _p1point < 3)
             {
-                if (p1point == 1)
-                    p1res = "Fifteen";
-                if (p1point == 2)
-                    p1res = "Thirty";
-                if (p1point == 3)
-                    p1res = "Forty";
+                if (_p1point < 3)
+                {
+                    score = GetScoreString(_p1point);
+                    score += "-All";
+                }
 
-                p2res = "Love";
-                score = p1res + "-" + p2res;
-            }
-            if (p2point > 0 && p1point == 0)
-            {
-                if (p2point == 1)
-                    p2res = "Fifteen";
-                if (p2point == 2)
-                    p2res = "Thirty";
-                if (p2point == 3)
-                    p2res = "Forty";
-
-                p1res = "Love";
-                score = p1res + "-" + p2res;
+                if (_p1point > 2)
+                {
+                    score = "Deuce";
+                }
             }
 
-            if (p1point > p2point && p1point < 4)
-            {
-                if (p1point == 2)
-                    p1res = "Thirty";
-                if (p1point == 3)
-                    p1res = "Forty";
-                if (p2point == 1)
-                    p2res = "Fifteen";
-                if (p2point == 2)
-                    p2res = "Thirty";
-                score = p1res + "-" + p2res;
-            }
-            if (p2point > p1point && p2point < 4)
-            {
-                if (p2point == 2)
-                    p2res = "Thirty";
-                if (p2point == 3)
-                    p2res = "Forty";
-                if (p1point == 1)
-                    p1res = "Fifteen";
-                if (p1point == 2)
-                    p1res = "Thirty";
-                score = p1res + "-" + p2res;
-            }
+            CheckingPositiveScore(_p1point, _p2point, ref _p1res, ref _p2res, ref score);
 
-            if (p1point > p2point && p2point >= 3)
-            {
-                score = "Advantage player1";
-            }
+            CheckingPositiveScore(_p2point, _p1point, ref _p2res, ref _p1res, ref score);
 
-            if (p2point > p1point && p1point >= 3)
-            {
-                score = "Advantage player2";
-            }
+            CheckingAverageScore(_p1point, _p2point, ref _p1res, ref _p2res, ref score);
 
-            if (p1point >= 4 && p2point >= 0 && (p1point - p2point) >= 2)
-            {
-                score = "Win for player1";
-            }
-            if (p2point >= 4 && p1point >= 0 && (p2point - p1point) >= 2)
-            {
-                score = "Win for player2";
-            }
+            CheckingAverageScore(_p2point, _p1point, ref _p2res, ref _p1res, ref score);
+
+            SetAdvantagePlayer(_p1point, _p2point, 1, ref score);
+
+            SetAdvantagePlayer(_p2point, _p1point, 2, ref score);
+
+            SetWinner(_p1point, _p2point, 1, ref score);
+
+            SetWinner(_p2point, _p1point, 2, ref score);
+
             return score;
+        }
+
+        private void CheckingPositiveScore(int firstPoints, int secondPoints, ref string firstPRes, ref string secondPRes,
+            ref string score)
+        {
+            if (firstPoints > 0 && secondPoints == 0)
+            {
+                firstPRes = GetScoreString(firstPoints);
+                secondPRes = "Love";
+                score = firstPRes + "-" + secondPRes;
+            }
+        }
+
+        private void CheckingAverageScore(int firstPoints, int secondPoints, ref string firstPRes, ref string secondPRes,
+            ref string score)
+        {
+            if (firstPoints > secondPoints && firstPoints < 4)
+            {
+                firstPRes = GetScoreString(firstPoints);
+                secondPRes = GetScoreString(secondPoints);
+                score = firstPRes + "-" + secondPRes;
+            }
+        }
+
+        private void SetAdvantagePlayer(int firstPoints, int secondPoints, int playerNumber, ref string score)
+        {
+            if (firstPoints > secondPoints && secondPoints >= 3)
+            {
+                score = "Advantage player" + playerNumber;
+            }
+        }
+
+        private void SetWinner(int firstPoints, int secondPoints, int playerNumber, ref string score)
+        {
+            if (firstPoints >= 4 && secondPoints >= 0 && (firstPoints - secondPoints) >= 2)
+            {
+                score = "Win for player" + playerNumber;
+            }
+        }
+
+        private string GetScoreString(int score)
+        {
+            return score switch
+            {
+                0 => "Love",
+                1 => "Fifteen",
+                2 => "Thirty",
+                3 => "Forty",
+                _ => throw new InvalidOperationException("Invalid score")
+            };
         }
 
         public void SetP1Score(int number)
@@ -122,12 +125,12 @@ namespace Tennis
 
         private void P1Score()
         {
-            p1point++;
+            _p1point++;
         }
 
         private void P2Score()
         {
-            p2point++;
+            _p2point++;
         }
 
         public void WonPoint(string player)
